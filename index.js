d61974c83bdbc1871e3c4efd4181c1468886c045
Raviteja <ravitejagongada123@gmail.com>
2022-06-09 18:34:44 +0530

    v13

--- index.js
+++ index.js
@@ -29,21 +29,9 @@ app.use(methodOverride('_method'));
 //for MongoDB compass local database we use 
 // const MONGODB_URI = "mongodb://localhost:27017/College_App";
 
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    autoIndex: false, // Don't build indexes
-    poolSize: 10, // Maintain up to 10 socket connections
-    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    family: 4 // Use IPv4, skip trying IPv6
-  };
-//   {useNewUrlParser: true,useUnifiedTopology: true}
 
 //to connect MongoDB_URI that we mentioned in the .env file
-mongoose.connect(process.env.MONGODB_URI,options)
+mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser: true,useUnifiedTopology: true})
         .then(() => {
             console.log("Hurry,MongoDB Atlas connected!!");
             })
